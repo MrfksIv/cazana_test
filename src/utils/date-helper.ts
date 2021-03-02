60d157adbe187a75028d3ee369c2ef1b54eb63d9
Constantinos Morfakis <constantinos.m@redoleus.com>
2021-03-02 14:27:25 +0200

    refactored getYearsBetweenTwoDates to fix rounding errors / added .env file

--- src/utils/date-helper.ts
+++ src/utils/date-helper.ts
@@ -1,5 +1,16 @@
-const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
-const DAYS_IN_A_YEAR = 365;
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.ONE_DAY_IN_MS || !process.env.DAYS_IN_A_YEAR) {
+    throw new Error('Check for missing properties from .env: [ONE_DAY_IN_MS, DAYS_IN_A_YEAR]');
+}
+const ONE_DAY_IN_MS = parseInt(process.env.ONE_DAY_IN_MS, 10);
+const DAYS_IN_A_YEAR = parseInt(process.env.DAYS_IN_A_YEAR, 10);
+
+if (Number.isNaN(ONE_DAY_IN_MS) || Number.isNaN(DAYS_IN_A_YEAR)) {
+    throw new Error('Wrong property types in .env: [ONE_DAY_IN_MS, DAYS_IN_A_YEAR] must be integers');
+}
 
 export function parseDateInMs(dateString: string): number {
     const dateInMs = Date.parse(dateString);
@@ -9,9 +20,10 @@ export function parseDateInMs(dateString: string): number {
     return dateInMs;
 }
 
-export function getYearsBetweenTwoDates(dateInMs1: number, dateInMs2: number): number {
+export function getYearsBetweenTwoDates(dateInMs1: number, dateInMs2: number, decimalPlaces = 2): number {
     const diffInMs = dateInMs1 - dateInMs2;
     const diffInDays = diffInMs / ONE_DAY_IN_MS;
 
-    return diffInDays / DAYS_IN_A_YEAR;
+    // ignores leap-years but this will cause an insignificant error over the lifetime of a car
+    return +(diffInDays / DAYS_IN_A_YEAR).toFixed(decimalPlaces);
 }
